5225f4bf1a06d0e383c7a7fd7e3aa0e1a6a24a81
Claudia <claudiamary.santos@gmail.com>
2019-07-14 14:16:38 -0300

    add metodo get dinamico filmes

--- assets/js/script-perfil.js
+++ assets/js/script-perfil.js
@@ -1,3 +1,4 @@
+const container = document.querySelector('.catalogo')
 fetch('http://localhost:3000/usuario/filmes', {
         method: 'GET',
         headers: {
@@ -11,4 +12,31 @@ fetch('http://localhost:3000/usuario/filmes', {
     })
     .then(data => {
         console.log(data);
+        data.forEach(filme => {
+            const card = document.createElement('div');
+            card.setAttribute('class', 'catalogo-pai');
+
+            const titulo = document.createElement('p');
+            titulo.innerHTML = filme.nome;
+
+            const imagem = document.createElement('img');
+            imagem.setAttribute('class', 'catalogo-pai-img');
+            imagem.setAttribute('src', filme.imagem)
+
+            const estrela = document.createElement('div')
+            estrela.setAttribute('class', 'catalogo-pai-estrela');
+
+            card.appendChild(titulo);
+            card.appendChild(imagem);
+            for (let i = 0; i < filme.estrelas; i++) {
+                const estrelaimg = document.createElement('img');
+                estrelaimg.setAttribute('class', 'img-estrela')
+                estrelaimg.setAttribute('src', 'assets/img/star.png');
+                estrela.appendChild(estrelaimg);
+            }
+            card.appendChild(estrela);
+            container.appendChild(card);
+        });
     })
+
+const botaoCadastrar = document.querySelector('.btn-cadastro-filme');
